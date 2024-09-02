efcaafde8acf00ec51a76d0ef6b84095ca0730ed
bivsth88 <bivsth88@gmail.com>
2024-09-02 13:57:48 -0400

    1st application

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello there $name!",
+        text = "Hello there! $name!",
         modifier = modifier
     )
 }
